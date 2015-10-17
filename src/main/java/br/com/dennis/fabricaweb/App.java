5adf7ec5f6f6c1598c384abe42e03b95e143193c
DennisAlves <dennis.alves@outlook.com>
2015-10-17 15:19:44 -0300

    meu primeiro commit

---
+++ src/main/java/br/com/dennis/fabricaweb/App.java
@@ -0,0 +1,13 @@
+package br.com.dennis.fabricaweb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
